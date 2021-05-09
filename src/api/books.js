ff570bf0d6f60290a3a0e3b91b4a8980ed629a82
t1k2a <maxtj1122@gmail.com>
2021-05-09 23:21:17 +0900

    add drag and drop

--- src/api/books.js
+++ src/api/books.js
@@ -1,9 +1,17 @@
 // 本リスト
 // いつかECサイトから取得する　
 const database = [
-    { id: 1, name: '商品A', price: 100, content: '商品A詳細', review: '面白かった' },
-    { id: 2, name: '商品B', price: 200, content: '商品B詳細', review: '楽しかった' },
-    { id: 3, name: '商品C', price: 300, content: '商品C詳細', review: 'すごかった'}
+    [
+        { id: 1, name: '商品A', price: 100, content: '商品A詳細', review: '面白かった' },
+        { id: 2, name: '商品B', price: 200, content: '商品B詳細', review: '楽しかった' },
+        { id: 3, name: '商品C', price: 300, content: '商品C詳細', review: 'すごかった'},
+    ],
+    [
+        { id: 4, name: '商品D', price: 100, content: '商品A詳細', review: '面白かった' },
+        { id: 5, name: '商品E', price: 200, content: '商品B詳細', review: '楽しかった' },
+        { id: 6, name: '商品F', price: 300, content: '商品C詳細', review: 'すごかった'},
+    ],
+
 ]
 
 // インポート先で使用できる関数をオブジェクトとしてまとめたもの
--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,28 +1,71 @@
+<style>
+pre {
+  text-align: start;
+  background: #2c3e50;
+  color: white;
+  padding: 10px;
+  font-weight: bold;
+}
+
+.item {
+  padding: 5px;
+}
+
+.container {
+  margin: auto;
+  width: 600px;
+  display: flex;
+  justify-content: space-around;
+}
+</style>
+
 <template>
     <!-- draggable属性(draggable=".item")を付与 -->
-    <draggable v-model="books" draggable=".item" handle=".handle" :animation="300">
-        <!-- ドラッグ可能にするためにclass属性(class="item")を付与 -->
-        <v-col v-for="{ id, name } in list" :key="id" cols="auto" class="item">
-            <router-link :to="`/book/${ id }`" class="handle">{{ name }}</router-link>
-        </v-col>
-
+    <div class="container">
+        <draggable v-model="database[0]" draggable=".item" group="books" handle=".handle" :animation="300">
+            <!-- ドラッグ可能にするためにclass属性(class="item")を付与 -->
+            <v-col v-for="{ id, name } in database[0]" :key="id" cols="auto" class="item">
+                <router-link :to="`/book/${ id }`" class="handle">{{ name }}</router-link>
+            </v-col>
+        </draggable>
+        <draggable v-model="database[1]" draggable=".item" group="books" handle=".handle" :animation="300">
+            <v-col v-for="{ id, name } in database[1]" :key="id" cols="auto" class="item">
+                <router-link :to="`/book/${ id }`" class="handle">{{ name }}</router-link>
+            </v-col>
+        </draggable>
+        <div><pre>{{formattedItem1}}</pre></div>
+        <div><pre>{{formattedItem2}}</pre></div>
+    </div>
         <!-- カード追加ボタン -->
         <!-- itemクラスが付与されていないためドラッグ不可 -->
-        <v-col cols="auto">
+        <!-- <v-col cols="auto">
             <v-btn>
                 <v-icon><mdi-plus></mdi-plus></v-icon>
             </v-btn>
-        </v-col>
-    </draggable>
+        </v-col> -->
 </template>
 
 <script>
+import  vue from 'vue'
 import  books  from '@/api/books'
 import  draggable from 'vuedraggable'
 
-export default {
+function dumpObj(obj){
+    return JSON.stringify(obj, null, 2)
+}
+
+const database = books.fetch();
+
+export default vue.extend({
+    name: "Swap",
     components: {
         draggable,
+    },
+    data() {
+        return {
+            inDrag: false,
+            database
+        }
     },
     computed: {
         // list: () => books.fetch()
@@ -33,8 +76,15 @@ export default {
             set(value) {
                 this.books.commit('updateList', value)
             },
+           
+        },
+        formattedItem1() {
+            return dumpObj(this.database[0])
+        },
+            formattedItem2() {
+            return dumpObj(this.database[1])
         },
     },
-}
+})
 // 本の詳細ページを作る
 </script>
